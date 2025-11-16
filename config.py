b9a274bb12132e872c16052f6140647be2798e34
arvind0251 <160855021+arvind0251@users.noreply.github.com>
2025-11-16 13:59:13 +0530

    Update config.py
    Signed-off-by: arvind0251 <160855021+arvind0251@users.noreply.github.com>

--- config.py
+++ config.py
@@ -174,7 +174,7 @@ DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
 if SUPPORT_CHANNEL:
     if not re.match("(?:http|https)://", SUPPORT_CHANNEL):
         raise SystemExit(
-            "[ERROR] - Your SUPPORT_CHANNEL url is wrong. Please ensure that it starts with https://"
+            "[ERROR] - Your SUPPORT_CHANNEL url is wrong. Please ensure that it start with https://"
         )
 
 if SUPPORT_CHAT:
